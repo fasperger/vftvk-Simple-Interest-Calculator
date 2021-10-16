315885868a5c846e3b32549156c1326175c28a8a
Franz Asperger <f.asperger@gmail.com>
2021-10-16 01:58:34 +0000

    error in sum

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
-    var amount = principal + interest;
+    var amount = parseFloat(principal) + parseFloat(interest);
 
     var year = new Date().getFullYear()+parseInt(years);
     
@@ -15,7 +15,7 @@ function compute()
 
 function updateRate()
 {
-    var rateval = document.getElementById("rate").value+" %";
+    var rateval = document.getElementById("rate").value+"%";
     document.getElementById("rate_val").innerText=rateval;
 }
         
