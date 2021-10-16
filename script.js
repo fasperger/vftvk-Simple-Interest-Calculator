4b356344464562587125fd32aa47f6728bd6116a
Franz Asperger <f.asperger@gmail.com>
2021-10-16 01:46:19 +0000

    changes in script

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    
     var rate = document.getElementById("rate").value;
-    
     var years = document.getElementById("years").value;
-    
     var interest = principal * years * rate / 100;
+    var amount = principal + interest;
 
     var year = new Date().getFullYear()+parseInt(years);
     
@@ -17,7 +15,7 @@ function compute()
 
 function updateRate()
 {
-    var rateval = document.getElementById("rate").value;
+    var rateval = document.getElementById("rate").value+" %";
     document.getElementById("rate_val").innerText=rateval;
 }
         
