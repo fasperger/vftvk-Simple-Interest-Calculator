f4711be22bca0047064485211c2eb7681edc643b
Franz Asperger <f.asperger@gmail.com>
2021-10-17 02:20:33 +0000

    changes

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function compute()
         return false;
     }
     else {
-        document.getElementById("interest").innerHTML=interest;
         document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>, \
         <br\>at an interest rate of <mark>"+rate+"%</mark>.\<br\>You will receive an amount of <mark>"+amount+"</mark>,\
         <br\>in the year <mark>"+year+"</mark>\<br\>"
