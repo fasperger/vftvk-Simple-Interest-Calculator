3429b5d3a419fc0ed461de72e271238cb652eddc
Franz Asperger <f.asperger@gmail.com>
2021-10-18 16:35:21 +0000

    fixed formula interest

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal.value * years * rate / 100;
-    var amount = parseFloat(principal.value) + parseFloat(interest);
+    
 
     var year = new Date().getFullYear()+parseInt(years);
     
@@ -17,7 +17,7 @@ function compute()
     // if number is positive and greater than zero, then it should display the text containing the resulting information
     else {
         document.getElementById("result").innerHTML="If you deposit <mark>"+principal.value+"</mark>, \
-        <br\>at an interest rate of <mark>"+rate+"%</mark>.\<br\>You will receive an amount of <mark>"+amount+"</mark>,\
+        <br\>at an interest rate of <mark>"+rate+"%</mark>.\<br\>You will receive an amount of <mark>"+interest+"</mark>,\
         <br\>in the year <mark>"+year+"</mark>\<br\>"
     }   
 }
